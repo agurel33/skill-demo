1242d469401862b17a41bc43841b985100475096
agurel33 <agurel@ucsd.edu>
2022-02-03 11:02:32 -0800

    created lib and a failing test case

---
+++ SkillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void squaring() {
+        assertEquals(4, SkillDemo.square(10));
+    }
+}
