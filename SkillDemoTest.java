2bfface6f7628e375241e387a0d43c5fb233b096
agurel33 <agurel@ucsd.edu>
2022-02-03 11:04:24 -0800

    fixed test case and should pass now

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void squaring() {
-        assertEquals(4, SkillDemo.square(10));
+        assertEquals(4, SkillDemo.square(2));
     }
 }
